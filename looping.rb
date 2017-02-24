3b385bdded48f070d6d41c6bee2c51c86d215191
Panagiotis Skarlas <pskarl@gmail.com>
2017-02-24 19:29:14 +0000

    Done.

--- looping.rb
+++ looping.rb
@@ -1,5 +1,17 @@
+require 'pry'
 def looping
+  counter = 0
 	#your code here
+  loop do
+    counter+=1
+    #binding.pry
+    puts "I will stop after #{(counter - 10).abs} times"
+    if counter == 9
+      puts "Bye bye"
+      break
+    end
+  end
 end
 
 #call your method here
+looping()
